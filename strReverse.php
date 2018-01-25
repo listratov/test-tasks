9b03d5efab0e504ea33c5ac97e1af4dcce3bdf6d
listratov <listratov.m@gmail.com>
2018-01-25 07:30:54 +0300

    Update strReverse.php

--- strReverse.php
+++ strReverse.php
@@ -1,3 +1,8 @@
+
+/**
+*
+* 1 way
+**/
 $a = 'abcdefgh';
 
 $b = '';
@@ -6,4 +11,21 @@ for ($i = strlen($a)-1; $i>=0; $i--) {
 }
 
 $a = $b;
+print_r($a);
+
+/**
+*
+* 2 way
+**/
+
+$a = 'abcdefgh';
+
+$b = '';
+$l  = strlen($a)-1;
+for ($i = 0; $i < $l/2; $i++) {
+    $b = $a[$i];
+    $a[$i] = $a[$l-$i];
+    $a[$l-$i] = $b;
+}
+
 print_r($a);
